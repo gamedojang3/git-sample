39b67cf5a833c9abfad76d81b3044aefa18561ac
gamedojang <gamedojang@gmail.com>
2025-10-23 14:45:19 +0900

    GameManager 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,12 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Map3");
+        Debug.Log("Player4");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Player4");
     }
 }
